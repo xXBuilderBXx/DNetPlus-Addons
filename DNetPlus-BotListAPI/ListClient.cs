274443c03d697f649976fef7406a5e8feca01d94
builderb gamer <builderbgamer@gmail.com>
2020-10-30 21:45:29 +0000

    Add BotListAPI support.

---
+++ DNetPlus-BotListAPI/ListClient.cs
@@ -0,0 +1,151 @@
+﻿using Discord.WebSocket;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace BotListAPI
+{
+    /// <summary> BotListAPI client to post server count </summary>
+    public class ListClient
+    {
+        public readonly ListConfig Config = new ListConfig();
+
+        /// <summary> Discord client can be normal or sharded </summary>
+        public readonly BaseSocketClient Discord;
+
+        private readonly Timer Timer;
+
+        /// <summary> Log event for when a log message is triggered </summary>
+        public Action<LogType, string> MessageLog;
+
+        private HttpClient Http;
+
+        /// <summary> Log type for auto posting </summary>
+        public LogType LogType = LogType.Info;
+
+        public ListClient(BaseSocketClient client, ListConfig config)
+        {
+            if (config == null)
+                throw new ArgumentException("The list config cannot be null");
+            Discord = client;
+            Config = config;
+            if (Config.DiscordBotListv2 != "" && !Config.DiscordBotListv2.StartsWith("Bot"))
+                Config.DiscordBotListv2 = "Bot " + Config.DiscordBotListv2;
+
+            Timer = new Timer
+            {
+                Interval = 600000,
+                Enabled = false
+            };
+            Timer.Elapsed += Timer_Elapsed;
+        }
+
+        /// <summary> Disable all auto posting </summary>
+        public bool Disabled = false;
+
+        /// <summary> Start posting server count every 10 minutes </summary>
+        public void Start()
+        {
+            if (!Timer.Enabled)
+            {
+                Timer.Start();
+                Log(LogType.None, LogType.None, "Enabled auto posting server count");
+            }
+        }
+
+        /// <summary> Stop auto posting server count </summary>
+        public void Stop()
+        {
+            if (Timer.Enabled)
+            {
+                Timer.Stop();
+                Log(LogType.None, LogType.None, "Disabled auto posting server count");
+            }
+        }
+
+        /// <summary> Post server count every 10 minutes  </summary>
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            if (Discord != null && !Disabled)
+                _ = SendBotBlock(LogType);
+        }
+
+        public readonly string Version = "5.0";
+
+        /// <summary> Post server count to BotBlock.org </summary>
+        public async Task SendBotBlock(LogType type)
+        {
+            bool isError = false;
+            if (Http == null)
+            {
+                if (Discord == null)
+                {
+                    Log(type, LogType.Error, "Cannot post server count, Discord client is null");
+                    isError = true;
+                }
+                else if (Discord.CurrentUser == null)
+                {
+                    Log(type, LogType.Error, "Cannot post server count, CurrentUser is null");
+                    isError = true;
+                }
+                Http = new HttpClient();
+                Http.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                Http.DefaultRequestHeaders.Add("User-Agent", $"BotListAPI {Version} - " + Discord.CurrentUser.ToString());
+            }
+            if (!isError)
+            {
+                ListJson Json = Config.GetJson();
+                Json.SetCount(Discord);
+                
+                string JsonString = JsonConvert.SerializeObject(Json);
+                try
+                {
+                    StringContent Content = new StringContent(JsonString, Encoding.UTF8, "application/json");
+                    Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                    HttpResponseMessage Res = await Http.PostAsync("https://botblock.org/api/count", Content);
+                    if (Res.IsSuccessStatusCode)
+                    {
+                            Log(type, LogType.Info, $"Successfully posted server count to BotBlock");
+                            Log(type, LogType.Debug, "Request response in JSON\n" + JsonConvert.SerializeObject(Res, Formatting.Indented));
+                    }
+                    else
+                    {
+                        Log(type, LogType.Error, $"Error could not post server count to BotBlock, {(int)Res.StatusCode} {Res.ReasonPhrase}");
+                        Log(type, LogType.Debug, "Request response in JSON\n" + JsonConvert.SerializeObject(Res, Formatting.Indented));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Log(type, LogType.Error, $"Error could not post server count to BotBlock, {ex.Message}");
+                    Log(type, LogType.Debug, "Exception\n" + ex.ToString());
+                }
+            }
+        }
+
+        internal void Log(LogType type, LogType Match, string text)
+        {
+            MessageLog?.Invoke(type, text);
+            if (type >= Match)
+                Console.WriteLine("[BotListAPI] " + text);
+        }
+    }
+    
+    public enum LogType
+    {
+        /// <summary> Dont log anything to console </summary>
+        None,
+
+        /// <summary> Only log errors </summary>
+        Error,
+
+        /// <summary> Log success and errors </summary>
+        Info,
+
+        /// <summary> Log everything including request responses in json to console </summary>
+        Debug
+    }
+}
---
+++ DNetPlus-BotListAPI/ListConfig.cs
@@ -0,0 +1,116 @@
+﻿using Discord.WebSocket;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace BotListAPI
+{
+    public class ListConfig
+    {
+        [JsonProperty(PropertyName = "arcane-botcenter.xyz")]
+        public string ArcaneBotList { get; set; } = "";
+
+        [JsonProperty(PropertyName = "botlist.space")]
+        public string BotListSpace { get; set; } = "";
+
+        [JsonProperty(PropertyName = "bots.ondiscord.xyz")]
+        public string BotsOnDiscord { get; set; } = "";
+
+        [JsonProperty(PropertyName = "botsfordiscord.com")]
+        public string BotsForDiscord { get; set; } = "";
+
+        [JsonProperty(PropertyName = "cloud-botlist.xyz")]
+        public string CloudBotList { get; set; } = "";
+
+        [JsonProperty(PropertyName = "discord.boats")]
+        public string DiscordBoats { get; set; } = "";
+
+        [JsonProperty(PropertyName = "discord.bots.gg")]
+        public string DiscordBots { get; set; } = "";
+
+        [JsonProperty(PropertyName = "discordapps.dev")]
+        public string DiscordApps { get; set; } = "";
+
+        [JsonProperty(PropertyName = "discordbot.world")]
+        public string DiscordBotWorld { get; set; } = "";
+
+        [JsonProperty(PropertyName = "discordbotlist.com")]
+        public string DiscordBotListv2 { get; set; } = "";
+
+        [JsonProperty(PropertyName = "discordbots.fun")]
+        public string DiscordBotsFun { get; set; } = "";
+
+        [JsonProperty(PropertyName = "discordextremelist.xyz")]
+        public string DiscordExtremeList { get; set; } = "";
+
+        [JsonProperty(PropertyName = "divinediscordbots.com")]
+        public string DivineBotList { get; set; } = "";
+
+        [JsonProperty(PropertyName = "discordservices.net")]
+        public string DiscordServices { get; set; } = "";
+
+        [JsonProperty(PropertyName = "discordlist.co")]
+        public string DiscordListCo { get; set; } = "";
+
+        [JsonProperty(PropertyName = "glennbotlist.xyz")]
+        public string GlennBotlist { get; set; } = "";
+
+        [JsonProperty(PropertyName = "lbots.org")]
+        public string LBots { get; set; } = "";
+
+        [JsonProperty(PropertyName = "mythicalbots.xyz")]
+        public string MythicalBots { get; set; } = "";
+
+        [JsonProperty(PropertyName = "top.gg")]
+        public string TopGG { get; set; } = "";
+
+        [JsonProperty(PropertyName = "wonderbotlist.com")]
+        public string WonderBotList { get; set; } = "";
+
+        [JsonProperty(PropertyName = "yabl.xyz")]
+        public string YetAnotherBotList { get; set; } = "";
+
+        public ListJson GetJson()
+        {
+            ListJson Json = new ListJson();
+            PropertyDescriptorCollection sourceproperties = TypeDescriptor.GetProperties(new ListConfig());
+            PropertyDescriptorCollection targetproperties = TypeDescriptor.GetProperties(new ListJson());
+            foreach (PropertyDescriptor pd in targetproperties)
+                foreach (PropertyDescriptor _pd in sourceproperties)
+                    if (pd.Name == _pd.Name)
+                        pd.SetValue(Json, _pd.GetValue(this));
+            return Json;
+        }
+    }
+
+    public class ListJson : ListConfig
+    {
+        public void SetCount(BaseSocketClient client)
+        {
+            if (client.CurrentUser != null)
+            {
+                server_count = client.Guilds.Count;
+                bot_id = client.CurrentUser.Id.ToString();
+
+                if (client is DiscordShardedClient ds)
+                {
+                    shard_count = ds.Shards.Count;
+                    List<int> ShardTemp = new List<int>();
+                    foreach (DiscordSocketClient s in ds.Shards)
+                    {
+                        ShardTemp.Add(s.Guilds.Count);
+                    }
+                    shards = ShardTemp;
+                }
+            }
+        }
+
+        public string bot_id = "";
+
+        public int server_count = 0;
+
+        public int shard_count = 0;
+
+        public List<int> shards = new List<int>();
+    }
+}
